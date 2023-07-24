863ea7b0cf3bf1e335d3702b4ddfbfb6e5a210f7
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-07-24 12:20:28 +0200

    firstCommit

---
+++ src/main/java/andrearuzittu/EpicodeItjava4D1/EpicodeItJava4D1Application.java
@@ -0,0 +1,13 @@
+package andrearuzittu.EpicodeItjava4D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeItJava4D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeItJava4D1Application.class, args);
+
+	}
+}
---
+++ src/test/java/andrearuzittu/EpicodeItjava4D1/EpicodeItJava4D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package andrearuzittu.EpicodeItjava4D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeItJava4D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
